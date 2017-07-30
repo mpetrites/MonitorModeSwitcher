4db9a45ecc3403ac4d0ac965c478938299d6649c
Matt Petrites <mpetrites@gmail.com>
2017-07-30 09:22:00 -0400

    Add MonitorChanger class, Main UI class
    Initial code commit.

---
+++ MonitorModeSwitcher/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MonitorModeSwitcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void All_Enabled(object sender, RoutedEventArgs e)
+        {
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.Primary), MonitorChanger.MonitorActions.EnableMonitor);
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.Secondary), MonitorChanger.MonitorActions.EnableMonitor);
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.TV), MonitorChanger.MonitorActions.EnableMonitor);
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.Primary), MonitorChanger.MonitorActions.SetPrimaryMonitor);
+        }
+
+        private void Desktop_Mode(object sender, RoutedEventArgs e)
+        {
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.Primary), MonitorChanger.MonitorActions.EnableMonitor);
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.Secondary), MonitorChanger.MonitorActions.EnableMonitor);
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.Primary), MonitorChanger.MonitorActions.SetPrimaryMonitor);
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.TV), MonitorChanger.MonitorActions.DisableMonitor);
+        }
+
+        private void Game_Mode(object sender, RoutedEventArgs e)
+        {
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.Primary), MonitorChanger.MonitorActions.EnableMonitor);
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.Primary), MonitorChanger.MonitorActions.SetPrimaryMonitor);
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.Secondary), MonitorChanger.MonitorActions.DisableMonitor);
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.TV), MonitorChanger.MonitorActions.DisableMonitor);
+        }
+
+        private void TV_Mode(object sender, RoutedEventArgs e)
+        {
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.TV), MonitorChanger.MonitorActions.EnableMonitor);
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.TV), MonitorChanger.MonitorActions.SetPrimaryMonitor);
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.Primary), MonitorChanger.MonitorActions.DisableMonitor);
+            MonitorChanger.AlterMonitor(int.Parse(Properties.Resources.Secondary), MonitorChanger.MonitorActions.DisableMonitor);
+        }
+    }
+}
---
+++ MonitorModeSwitcher/MonitorSwitcher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MonitorModeSwitcher
+{
+    public static class MonitorChanger
+    {
+        //see http://www.nirsoft.net/utils/multi_monitor_tool.html for more
+        public enum MonitorActions
+        {
+            DisableMonitor = 0,
+            EnableMonitor = 1,
+            SetPrimaryMonitor = 2
+        }
+
+        public static void AlterMonitor(int id, MonitorActions action)
+        {
+            switch (action)
+            {
+                case (MonitorActions.DisableMonitor):
+                    RunProcess($"/disable {id}");
+                    break;
+
+                case (MonitorActions.EnableMonitor):
+                    RunProcess($"/enable {id}");
+                    break;
+
+                case (MonitorActions.SetPrimaryMonitor):
+                    RunProcess($"/SetPrimary {id}");
+                    break;
+            }
+        }
+
+        private static void RunProcess(string command)
+        {
+            using (System.Diagnostics.Process process = new System.Diagnostics.Process())
+            {
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "MultiMonitorTool.exe";
+                startInfo.Arguments = command;
+                process.StartInfo = startInfo;
+                process.Start();
+            }
+        }
+    }
+}
